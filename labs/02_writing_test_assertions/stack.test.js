459a05f7f9d54daa9be088be85803df7a38db251
harsh <harsh.singh@skillup.tech>
2025-05-07 01:17:27 +0530

    Reorder test cases and add package-lock.json for stack implementation

--- labs/02_writing_test_assertions/stack.test.js
+++ labs/02_writing_test_assertions/stack.test.js
@@ -13,17 +13,15 @@ describe('Stack', () => {
     stack = null;
   });
 
-  test.todo('push should add an item to the stack');
-
-  test.todo('pop should remove and return the top item from the stack');
-
-  test.todo('peek should return the top item without removing it');
-
   test.todo('isEmpty should return true when stack is empty');
 
   test.todo('isEmpty should return false when stack has items');
 
-  test.todo('pop should return undefined when stack is empty');
+  test.todo('peek should return the top item without removing it');
 
-  test.todo('peek should return undefined when stack is empty');
+  test.todo('push should add an item to the stack');
+
+  test.todo('pop should remove and return the top item from the stack');
+
+
 });
