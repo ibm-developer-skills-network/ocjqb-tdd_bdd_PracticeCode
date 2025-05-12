d3cb4ef930a5b903e9dcdaf9aaad54845b3c8f9d
harsh <harsh.singh@skillup.tech>
2025-05-13 04:42:22 +0530

    Add cucumber report and package files, remove setup feature and web steps

--- labs/08_environment_setup/features/steps/web_steps.js
+++
@@ -1,6 +0,0 @@
-/**
- * Web Steps
- */
-const { Given, When, Then } = require('@cucumber/cucumber');
-const { Builder, By, until } = require('selenium-webdriver');
-const assert = require('assert');
