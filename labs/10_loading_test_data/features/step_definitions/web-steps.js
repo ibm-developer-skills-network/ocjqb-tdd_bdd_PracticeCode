e2eecf20db695b9331c73cbb8efe482cc4d7f024
harsh <harsh.singh@skillup.tech>
2025-06-09 00:47:03 +0530

    feat: implement data loading step to refresh pets database with test data

--- labs/10_loading_test_data/features/step_definitions/web-steps.js
+++ labs/10_loading_test_data/features/step_definitions/web-steps.js
@@ -1,5 +1,6 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { By, until } = require('selenium-webdriver');
+const assert = require('assert');
 
 const ID_PREFIX = 'pet_';
 
@@ -25,7 +26,7 @@ Then('I should see the message {string}', async function (message) {
     until.elementTextContains(this.driver.findElement(By.id('flash_message')), message),
     this.waitSeconds
   );
-  expect(found).toBeTruthy();
+  assert.ok(found, 'Expected to find the message but did not');
 });
 
 Then('I should see {string} in the results', async function (name) {
@@ -33,11 +34,11 @@ Then('I should see {string} in the results', async function (name) {
     until.elementTextContains(this.driver.findElement(By.id('search_results')), name),
     this.waitSeconds
   );
-  expect(found).toBeTruthy();
+  assert.ok(found, 'Expected to find the name in results but did not');
 });
 
 Then('I should not see {string} in the results', async function (name) {
   const element = await this.driver.findElement(By.id('search_results'));
   const text = await element.getText();
-  expect(text).not.toContain(name);
+  assert.ok(!text.includes(name), `Expected not to find ${name} in results but did`);
 });
