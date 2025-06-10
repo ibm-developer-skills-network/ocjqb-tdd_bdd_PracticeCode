55da89b0a72c75c61b2e431f6695f9cefa7d9728
harsh <harsh.singh@skillup.tech>
2025-06-11 03:13:57 +0530

    refactor: remove empty try-catch block from pets loading step

--- labs/10_loading_test_data/features/step_definitions/load-steps.js
+++ labs/10_loading_test_data/features/step_definitions/load-steps.js
@@ -3,11 +3,5 @@ const axios = require('axios');
 const assert = require('assert');
 
 Given('the following pets', async function (dataTable) {
-  try {
-    // TODO
-   
-  } catch (error) {
-    console.error('Error in loading test data:', error.message);
-    throw error;
-  }
+  // Add try catch
 });
